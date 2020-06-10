11311472ab51118844be593633ad147cf5345e68
PhoenixRigo <39709352+PhoenixRigo@users.noreply.github.com>
2020-06-10 08:33:39 +0200

    menu

--- The_Game/game_menu/main.js
+++ The_Game/game_menu/main.js
@@ -16,13 +16,16 @@ var games_names_elem = document.getElementsByClassName('games_names')[0];
 
 
 
+
 // generation :
 generate();
+
+games_names_elem.style = `width:${(games_names.length + 1)*100}vw;`;
 
 
 function generate() {
     for (i = 0; i < games_names.length; i++) {
-        games_names_elem.innerHTML += `<h1>${games_names[i]}</h1>`;
+        games_names_elem.innerHTML += `<h2><a href="../games/${games_names[i]}/index.html">${games_names[i]}</a></h2>`;
     }
 }
 
